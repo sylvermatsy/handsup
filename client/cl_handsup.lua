0b8568469b64a68e84f17dcb2a0370972e83141a
SylverMATSY <103262166+sylvermatsy@users.noreply.github.com>
2022-08-18 18:51:16 +0200

    Update cl_handsup.lua

--- client/cl_handsup.lua
+++ client/cl_handsup.lua
@@ -1,22 +1,19 @@
-ESX = nil
-
+local handsup = false
 Citizen.CreateThread(function()
-local dict = "missminuteman_1ig_2"
-	RequestAnimDict(dict)
-	while not HasAnimDictLoaded(dict) do
-		Citizen.Wait(0)
-	end
-    local handsup = false
-	while true do
-		Citizen.Wait(0)
-		if IsControlJustPressed(1, Config.Touche) then
-            if not handsup then
-                TaskPlayAnim(PlayerPedId(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
-                handsup = true
-            else
-                handsup = false
-                ClearPedTasks(PlayerPedId(-1))
-            end
-        end
+    RequestAnimDict("missminuteman_1ig_2")
+    while not HasAnimDictLoaded("missminuteman_1ig_2") do
+        Wait(100)
+    end
+end)
+
+
+RegisterKeyMapping('main', Config.Desc, 'keyboard' Config.Touche)
+RegisterCommand('main', function()
+    if not handsup then
+        TaskPlayAnim(PlayerPedId(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+        handsup = true
+    else
+        handsup = false
+        ClearPedTasks(PlayerPedId(-1))
     end
 end)
