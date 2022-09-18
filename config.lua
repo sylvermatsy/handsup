2b6fd288d23bffad0e0d68f96f73e29b200499dd
SylverMATSY <103262166+sylvermatsy@users.noreply.github.com>
2022-09-18 20:25:46 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config                            = {}
 
-Config.Touche = 323 -- https://docs.fivem.net/docs/game-references/controls/
+Config.Touche = X -- https://docs.fivem.net/docs/game-references/controls/
 
 Config.Desc = Lever Les Mains
